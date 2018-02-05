f45680757d588180087a6e2a2b326fa84ffd05eb
kennerus <kennerus@gmail.com>
2018-02-05 13:50:10 +0300

    catalogue 5

---
+++ js/count.js
@@ -0,0 +1,31 @@
+$('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+
+$('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
+$('.minus-p').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1 + '%';
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+
+$('.plus-p').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1 + '%');
+    $input.change();
+    return false;
+});
