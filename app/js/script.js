f3c0faf5b92e43f8a5b7a2563f3b1d0b8d5876c1
kennerus <kennerus@gmail.com>
2018-02-08 16:41:46 +0300

    all modals

--- app/js/script.js
+++ app/js/script.js
@@ -44,6 +44,37 @@ if (idDelivery) {
     replaceSubject(idDelivery, 'filter__select_list', 'filter__select_active');
 }
 
+//tabs 
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove('db');
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" modal__nav_active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).classList.add('db')
+    evt.currentTarget.className += " modal__nav_active";
+}
+
+// // Get the element with id="defaultOpen" and click on it
+// var defaultOpen = document.getElementsByClassName("defaultOpen");
+
+
+
+// if(defaultOpen) {
+//     defaultOpen.click();
+// }
+
 
 $(function () {
 
