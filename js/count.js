2685c6195aad6c515e335fe6321b63dfac343ac9
kennerus <kennerus@gmail.com>
2018-02-22 16:22:27 +0300

    fixes

--- js/count.js
+++ js/count.js
@@ -1,32 +1,48 @@
-$('.minus').click(function () {
+$(document).on('click', '.minus', function () {
     var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
+    var count = parseInt($input.val()) - 10;
+    count = count <= 1 ? 0 : count;
     $input.val(count);
     $input.change();
     return false;
 });
 
-$('.plus').click(function () {
+$(function() {
+    var countInputs = $('.catalogue-pre__number-preorder input');
+    countInputs.each(function() {
+        if($(this).val() === '') {
+            $(this).val('0');
+        }
+    })
+})
+
+$(document).on('click', '.plus', function () {
     var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1);
+    $input.val(parseInt($input.val()) + 10);
     $input.change();
     return false;
 });
 
-$('.minus-p').click(function () {
+$(document).on('click', '.minus-p', function () {
     var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1 + '%';
-    count = count < 1 ? 1 : count;
-    $input.val(count);
+    var count = parseInt($input.val()) - 10;
+    var counter = count + '%';
+    if(count <= 0) {
+        console.log(123)
+        $input.val('0');
+    }
+    else {
+        $input.val(counter);
+    }
     $input.change();
     return false;
 });
 
-$('.plus-p').click(function () {
+$(document).on('click', '.plus-p', function () {
     var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1 + '%');
+    var count = parseInt($input.val()) + 10;
+    var counter = count + '%';
+    $input.val(counter);
     $input.change();
     return false;
 });
-
